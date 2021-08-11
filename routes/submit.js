0e9fbf139d79b58e6ad3a5ef1ae17addf398df90
Vidhi Khaitan <vidhik2002@gmail.com>
2021-08-11 18:58:44 +0530

    feat: update submit route

--- routes/submit.js
+++ routes/submit.js
@@ -26,49 +26,115 @@ router.get('/',validator.body(authUserSchema), async (req, res) => {
       });
     }
 
-    if(quesId === nodeInfo.lockedNode){
-        if(answer[0] === result.answer[0]){
-            res.json({
-                message: "answer correct"
-            })
-            const obj = JSON.parse(await readfile("./models/questions.json"));
-            const p = obj[quesId.toString()].adjacent;
-            
-                nodeInfo.solvedNodes.push(quesId)
-                //not starting node 37,38,39
-                if(result.isStarting === false){
-                    for (let i = 0; i < p.length; i++) {
-                        nodeInfo.unlockedNodes.push(p[i]);
-                    }
-                }else{
-                    console.log("starting node")
-                    
-                    var starting = [37, 38, 39];
-                    starting.map((id) => {
-                        if (id != quesId) {
-                        nodeInfo.unlockedNodes.pop(id);
+    // if(!nodeInfo.solvedNodes.includes(quesId) || result.isPortal){
+
+        if(quesId === nodeInfo.lockedNode || result.isStarting ){
+    
+            if(answer[0] === result.answer[0]){
+                
+                const obj = JSON.parse(await readfile("./models/questions.json"));
+                const p = obj[quesId.toString()].adjacent;
+    
+                        async function recursion(quesId) {
+                            const obj = JSON.parse(
+                                await readfile("./models/questions.json")
+                            );
+
+                            if (result.isPortal === true) {
+                                if (
+                                result.answer[0].includes(ans[0]) &&
+                                result.answer[1].includes(ans[1])
+                                ) {
+                                console.log("both correct ans");
+                                console.log(quesId);
+                                const p = obj[quesId.toString()].portal;
+                                console.log(p);
+                                async function gotoPortal(quesId) {
+                                    for (let i = 0; i < p.length; i++) {
+                                    if (isChecked.includes(p[i])) {
+                                        console.log(`already checked${p[i]}`);
+                                    } else if (
+                                        !nodeInfo.solvedNodes.includes(p[i])
+                                    ) {
+                                        console.log(`unlocked${p[i]}`);
+                                        nodeInfo.unlockedNodes.push(p[i]);
+                                        isChecked.push(p[i]);
+                                    } else if (
+                                        nodeInfo.solvedNodes.includes(p[i])
+                                    ) {
+                                        console.log(`solved${p[i]}`);
+                                        isChecked.push(p[i]);
+                                    } else {
+                                        console.log(`${p[i]}couldnot be processed`);
+                                    }
+                                    }
+                                }
+                                await gotoPortal(quesId);
+                                console.log("end of portal recursion");
+                            }
+                            }
+
+                            const q = obj[quesId.toString()].adjacent;
+
+                            for (let i = 0; i < q.length; i++) {
+                                console.log(q[i]);
+                                if (isChecked.includes(q[i])) {
+                                console.log(`already checked${q[i]}`);
+                                } else if (!nodeInfo.solvedNodes.includes(q[i])) {
+                                console.log(`unlocked${q[i]}`);
+                                nodeInfo.unlockedNodes.push(q[i]);
+                                isChecked.push(q[i]);
+                                } else if (nodeInfo.solvedNodes.includes(q[i])) {
+                                console.log(`solved${q[i]}`);
+                                isChecked.push(q[i]);
+                                await recursion(q[i]);
+                                } else {
+                                console.log(`${q[i]}couldnot be processed`);
+                                }
+                            }
                         }
-                    });
-                    nodeInfo.unlockedNodes.pop(quesId);
-                    for (let i = 0; i < p.length; i++) {
-                        nodeInfo.unlockedNodes.push(p[i]);
-                    }
-                    console.log(nodeInfo.unlockedNodes);
-                }
-            
-                player.score += result.points; //irrespective of being bridge question or not
-                nodeInfo.save();
-                player.save();
-            
-        }else{
+                
+                //not starting node 37,38,39
+                // if(!result.isStarting){
+                    //     for (let i = 0; i < p.length; i++) {
+                        //         nodeInfo.unlockedNodes.push(p[i]);
+                        //     }
+                        // }else{
+                        //     console.log("starting node")
+                        
+                        //     var starting = [37, 38, 39];
+                        //     starting.map((id) => {
+                            //         if (id != quesId) {
+                        //         nodeInfo.unlockedNodes.pop(id);
+                        //         }
+                        //     });
+                        //     nodeInfo.unlockedNodes.pop(quesId);
+                        //     for (let i = 0; i < p.length; i++) {
+                            //         nodeInfo.unlockedNodes.push(p[i]);
+                            //     }
+                            //     console.log(nodeInfo.unlockedNodes);
+                            // }
+                            
+                        nodeInfo.solvedNodes.push(quesId)
+                        player.score += result.points; //irrespective of being bridge question or not
+                        nodeInfo.save();
+                        player.save();
+                    
+                    //}
+            }else{
+                res.json({
+                    message: "answer not correct"
+                })
+            }
+        } else {
             res.json({
-                message: "answer not correct"
+                message: "the current question is not locked"
             })
         }
-    } else {
-        res.json({
-            message: "the current question is not locked"
-        })
-    }
+    // }else{
+    //     res.json({
+    //         message: "the current question is already solved"
+    //     })
+    // }
 });
 module.exports = router;
