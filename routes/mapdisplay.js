b93fd62a0c98a4edc90e07a27a06a5ac1cc7481e
salt57 <sourishgupta02@gmail.com>
2021-08-14 15:43:52 +0530

    merge: made get post

--- routes/mapdisplay.js
+++ routes/mapdisplay.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const map = require("../models/GameState");
 
 // ----------------------------Map Route-------------------------------
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   const { username } = req.body;
 
   try {
