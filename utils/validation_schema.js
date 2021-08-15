999fb06f21545eced31acd2e68fe26826b4dd615
salt57 <sourishgupta02@gmail.com>
2021-08-15 17:11:53 +0530

    fix: fixed JOI schema

--- utils/validation_schema.js
+++ utils/validation_schema.js
@@ -1,17 +1,14 @@
 const Joi = require('joi');
 
 const authPenaltySchema = Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(30).required(),
     quesId: Joi.number().integer().required(),
 });
 const authUserSchema = Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(30).required(),
     quesId: Joi.number().integer().required(),
     answer: Joi.array().items(Joi.string()).min(1).max(2)
        .required(),
 });
 const quesSchema = Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(30).required(),
     quesId: Joi.number().integer().required(),
 })
 
