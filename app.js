61b4a203ccee5675298786f2a0766a9bc45db8fa
Vidhi Khaitan <vidhik2002@gmail.com>
2021-08-18 08:58:35 +0530

    fix: refactor code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 
 require('dotenv/config');
+require('./models/dbInit');
 require('./utils/resetPenaltyPoints');
 
 app.use(cors());
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const authMiddleware = require('./middleware/authorize')
 
-const testRoute = require('./routes/testroute');
 const scoreRoute = require('./routes/leaderboard');
 const submitRoute = require('./routes/submit');
 const mapRoute = require('./routes/mapdisplay');
@@ -30,7 +29,7 @@ const apiLimiter = rateLimit({
     max: 100,
 });
 
-app.use('/test', testRoute);
+
 app.use('/score', apiLimiter, scoreRoute);
 app.use('/submit',authMiddleware, apiLimiter, submitRoute);
 app.use('/map', authMiddleware, apiLimiter, mapRoute);
@@ -40,35 +39,14 @@ app.use('/ques',authMiddleware , apiLimiter, quesRoute);
 app.use('/playerdata',authMiddleware , apiLimiter, playerdataRoute);
 app.use('/hint',authMiddleware , apiLimiter, hintRoute);
 
-app.get('/', (req, res) => {
-    res.send('homepage');
-});
-
-// Connection to the database
-mongoose.connect(
-    process.env.DB_CONNECTION,
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    },
-);
-
-mongoose.connection
-    .once('open', () => {
-        console.log('Connection to mongoDB established');
-    })
-    .on('error', (err) => {
-        console.log('Error connecting to mongoDB:', err);
-    });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: ({
-            message: error.message,
-        }),
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 app.listen(3001, () => {
