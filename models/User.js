3cada461f6b3112dcae21d346149d43711207f0e
Vidhi Khaitan <vidhik2002@gmail.com>
2021-08-18 13:26:35 +0530

    feat: refactor code

--- models/User.js
+++ models/User.js
@@ -1,28 +1,29 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    score: {
-        type: Number,
-    },
-    // currentPosition: {
-    //     type: Number,
-    //     required: true,
-    //     default: 0,
-    // },
-    currentPenaltyPoints: {
-        type: Number,
-        required: true,
-        default: 20,
-    },
-    // lockedNode: {
-    //     type: Number,
-    //     required: true,
-    // },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  // currentPosition: {
+  //     type: Number,
+  //     required: true,
+  //     default: 0,
+  // },
+  currentPenaltyPoints: {
+    type: Number,
+    required: true,
+    default: 20,
+  },
+  // lockedNode: {
+  //     type: Number,
+  //     required: true,
+  // },
 });
 const User = mongoose.model('user', UserSchema);
 module.exports = User;
