d099f1b23acf7753935bc3d9c3df2981215e93be
salt57 <sourishgupta02@gmail.com>
2021-08-19 15:52:21 +0530

    removed cors for testing

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ var corsOptions = {
   origin: 'http://example.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-
-app.use(cors({
-    origin: 'https://riddler.csivit.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+app.use(cors())
+// app.use(cors({
+//     origin: process.env.ORIGIN_URL,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }));
 app.set('trust proxy', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
